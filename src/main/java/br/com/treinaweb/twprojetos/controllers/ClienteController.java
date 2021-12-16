a8f0d8e31a93ae868a55f3127c7c007e674460bb
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-16 00:36:12 -0300

    feat: Creating the one-to-one relationship between the customer and address entities.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/ClienteController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.twprojetos.entities.Cliente;
+import br.com.treinaweb.twprojetos.enums.UF;
 import br.com.treinaweb.twprojetos.repositories.ClienteRepository;
 
 @Controller
@@ -41,6 +42,7 @@ public class ClienteController {
         ModelAndView modelAndView = new ModelAndView("cliente/formulario");
 
         modelAndView.addObject("cliente", new Cliente());
+        modelAndView.addObject("ufs", UF.values());
 
         return modelAndView;
     }
@@ -57,6 +59,7 @@ public class ClienteController {
         ModelAndView modelAndView = new ModelAndView("cliente/formulario");
 
         modelAndView.addObject("cliente", clienteRepository.getOne(id));
+        modelAndView.addObject("ufs", UF.values());
 
         return modelAndView;
     }
