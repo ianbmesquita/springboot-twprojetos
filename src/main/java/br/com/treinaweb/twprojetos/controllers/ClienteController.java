639b9e4424875ca6716cda6fdb7205a7286ae37e
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-15 22:51:01 -0300

    feat: Creating the remove customer action.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/ClienteController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.twprojetos.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,5 +23,12 @@ public class ClienteController {
         modelAndView.addObject("clientes", clienteRepository.findAll());
 
         return modelAndView;
+    }
+
+    @GetMapping(path="/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+
+        return "redirect:/clientes";
     }
 }
