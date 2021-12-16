2826cdcc1fe67522418673dc1cbf0e280ea0bf49
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-15 23:00:56 -0300

    feat: Creation of customer registration action.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/ClienteController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/ClienteController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.treinaweb.twprojetos.entities.Cliente;
 import br.com.treinaweb.twprojetos.repositories.ClienteRepository;
 
 @Controller
@@ -23,6 +25,22 @@ public class ClienteController {
         modelAndView.addObject("clientes", clienteRepository.findAll());
 
         return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("cliente/formulario");
+
+        modelAndView.addObject("cliente", new Cliente());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String salvar(Cliente cliente) {
+        clienteRepository.save(cliente);
+
+        return "redirect:/clientes";
     }
 
     @GetMapping(path="/{id}/excluir")
