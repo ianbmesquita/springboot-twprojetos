e3570629f5101fa57b88746aeac647d366760459
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-17 15:32:58 -0300

    feat: Creation of employee detail action.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/FuncionarioController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/FuncionarioController.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.twprojetos.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,6 +20,14 @@ public class FuncionarioController {
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView("/funcionario/home");
         modelAndView.addObject("funcionarios", funcionarioRepository.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detail(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("/funcionario/detail");
+        modelAndView.addObject("funcionario", funcionarioRepository.getOne(id));
 
         return modelAndView;
     }
