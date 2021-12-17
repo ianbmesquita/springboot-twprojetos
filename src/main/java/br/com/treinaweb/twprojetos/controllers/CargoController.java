d58e17858f4d8c28192e04e1f28ec6e41130d55d
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-17 16:18:37 -0300

    feat: Creation of position registration action.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/CargoController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/CargoController.java
@@ -3,9 +3,11 @@ package br.com.treinaweb.twprojetos.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.treinaweb.twprojetos.entities.Cargo;
 import br.com.treinaweb.twprojetos.repositories.CargoRepository;
 
 @Controller
@@ -21,5 +23,20 @@ public class CargoController {
         modelAndView.addObject("cargos", cargoRepository.findAll());
 
         return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("/cargo/formulario");
+        modelAndView.addObject("cargo", new Cargo());
+
+        return modelAndView;
+    }
+
+    @PostMapping({"/cadastrar"})
+    public String cadastrar(Cargo cargo) {
+        cargoRepository.save(cargo);
+
+        return "redirect:/cargos";
     }
 }
