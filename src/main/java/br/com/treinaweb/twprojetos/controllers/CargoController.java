8864ef158e5de5c8cae629c8a25e172134bbb437
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-17 16:21:36 -0300

    feat: Creation of position edition action.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/CargoController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/CargoController.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.twprojetos.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,10 +34,18 @@ public class CargoController {
         return modelAndView;
     }
 
-    @PostMapping({"/cadastrar"})
+    @PostMapping({"/cadastrar", "/{id}/editar"})
     public String cadastrar(Cargo cargo) {
         cargoRepository.save(cargo);
 
         return "redirect:/cargos";
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("/cargo/formulario");
+        modelAndView.addObject("cargo", cargoRepository.getOne(id));
+
+        return modelAndView;
     }
 }
