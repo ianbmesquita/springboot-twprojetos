b7a4f189058cc0b90d8bf262b2a8da7ad5644d35
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-18 13:29:26 -0300

    feat: Defining custom queries with @Query annotation.

--- src/main/java/br/com/treinaweb/twprojetos/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/treinaweb/twprojetos/repositories/FuncionarioRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.treinaweb.twprojetos.entities.Funcionario;
 
@@ -11,5 +12,15 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> 
 
     @EntityGraph(attributePaths = {"endereco", "cargo"})
     List<Funcionario> findAll();
+
+    @Query("select f from Funcionario f where f.cargo.nome = :nomeCargo")
+    List<Funcionario> buscarPorCargo(String nomeCargo);
+
+    @Query("select f from Funcionario f where f.cargo.nome <> :nomeCargo")
+    List<Funcionario> buscarPorCargoExceto(String nomeCargo);
+
+    // Buscas usando as keywords - O Spring interpreta pelo nome do método (Funcionario -> Cargo -> nome)
+    // List<Funcionario> findByCargoNome(String nomeCargo);
+    // List<Funcionario> findByCargoNomeNot(String nomeCargo);
     
 }
