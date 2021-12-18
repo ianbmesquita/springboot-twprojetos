a054ebb5cfa87ff997861bf108be186675ee12b7
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-18 13:04:56 -0300

    feat: Solving the n + 1 query problem by overriding the method in the repository class.

--- src/main/java/br/com/treinaweb/twprojetos/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/treinaweb/twprojetos/repositories/FuncionarioRepository.java
@@ -1,9 +1,15 @@
 package br.com.treinaweb.twprojetos.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.twprojetos.entities.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    @EntityGraph(attributePaths = {"endereco", "cargo"})
+    List<Funcionario> findAll();
     
 }
--- src/main/java/br/com/treinaweb/twprojetos/repositories/ProjetoRepository.java
+++ src/main/java/br/com/treinaweb/twprojetos/repositories/ProjetoRepository.java
@@ -1,9 +1,15 @@
 package br.com.treinaweb.twprojetos.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.twprojetos.entities.Projeto;
 
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+
+    @EntityGraph(attributePaths = {"cliente", "lider"})
+    List<Projeto> findAll();
     
 }
