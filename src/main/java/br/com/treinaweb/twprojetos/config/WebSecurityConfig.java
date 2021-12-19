1c98421afef8e1f05f194f6f9b400e8827404ced
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-19 02:21:45 -0300

    feat: Configuring the logout action.

--- src/main/java/br/com/treinaweb/twprojetos/config/WebSecurityConfig.java
+++ src/main/java/br/com/treinaweb/twprojetos/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.treinaweb.twprojetos.services.UserDetailServiceImpl;
 
@@ -29,6 +30,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .defaultSuccessUrl("/clientes")
                 .permitAll();
+
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .logoutSuccessUrl("/login");
     }
 
     @Override
