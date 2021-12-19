130f6af380bbeef92d2e099c60b2d631a63e185a
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-19 02:10:52 -0300

    feat: Creating the controller of users to make login.

---
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/UsuarioController.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.twprojetos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+    
+    @GetMapping("/login")
+    public String login() {
+        return "usuario/login";
+    }
+}
