bafcaba985347d700bacdf47bf85102af8a2656d
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-28 18:47:57 -0300

    feat: Creating the user profile page.

--- src/main/java/br/com/treinaweb/twprojetos/controllers/UsuarioController.java
+++ src/main/java/br/com/treinaweb/twprojetos/controllers/UsuarioController.java
@@ -1,13 +1,32 @@
 package br.com.treinaweb.twprojetos.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.twprojetos.entities.Funcionario;
+import br.com.treinaweb.twprojetos.repositories.FuncionarioRepository;
 
 @Controller
 public class UsuarioController {
-    
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
     @GetMapping("/login")
     public String login() {
         return "usuario/login";
+    }
+
+    @GetMapping("/perfil")
+    public ModelAndView perfil(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("usuario/perfil");
+
+        Funcionario usuario = funcionarioRepository.findByEmail(principal.getName()).get();
+        modelAndView.addObject("usuario", usuario);
+
+        return modelAndView;
     }
 }
